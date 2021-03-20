228a3e622d55a187fc8149aa341210da746f1ddb
julianbooher <julianbooher@hotmail.com>
2021-03-20 17:19:23 -0500

    initialized model for transactions router

---
+++ server/models/transactions.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({ 
+  name: {
+    type: String,
+    required: [true, "Name is required"]
+  },
+  date: {
+    type: Date,
+    required: [true, "Date can't be blank"]
+  },
+  cost: {
+      type: Number,
+      required: [true, "Cost is required"]
+  }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
-const express = require('express');
-const app = express();
-const PORT = 3001;
+const express = require('express'); 
+const mongoose = require('mongoose');
+// const cors = require('cors');
+const router = require('./transactions');
+require('dotenv').config()
 
-app.get('/', function(request, response) { response.send('Hello World!') });
+const app = express(); 
+const PORT = 3001; 
+const MONGODB_URI = `mongodb+srv://julianbooher:${process.env.MONGO_CLUSTER_PW}@julianbooher.o1lfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`; 
 
-app.listen(PORT, function() { console.log(`Server listening on port ${PORT}`) });
+// app.use(cors())
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use('/api', router); 
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
+mongoose.connection.once('open', ()=> { 
+  console.log('Connected to the Database.');
+});
+mongoose.connection.on('error', (error)=> {
+  console.log('Mongoose Connection Error : ' + error);
+});
+
+app.listen(PORT, ()=> { 
+  console.log(`Server listening on port ${PORT}.`);
+});
